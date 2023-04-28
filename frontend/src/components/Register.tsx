7efcb0947b9a13dae58fd949bfa955fcb0e1a828
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-28 19:37:30 +0200

    header + style

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react"
+import { useState } from "react"
+import "../styles/Register.css"
 
 const Register = () => {
 	const [username, setUsername] = useState("")
@@ -8,7 +9,11 @@ const Register = () => {
 	async function handleSubmit() {
 		const res = await fetch("/api/user/create", {
 			method: "POST",
-			body: JSON.stringify({ username, password, email }),
+			body: JSON.stringify({
+				username,
+				password,
+				email
+			}),
 			headers: { "Content-Type": "application/json" }
 		})
 		if (res.ok) {
@@ -18,11 +23,15 @@ const Register = () => {
 	}
 
 	return (
-		<div>
-			<input type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
-			<input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-			<input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
-			<button onClick={handleSubmit}></button>
+		<div className="grid h-full">
+			<div className="register">
+				<input className="register-input" type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+				<input className="register-input" type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+				<input className="register-input" type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+				<button className="register-button" onClick={handleSubmit}>
+					Login
+				</button>
+			</div>
 		</div>
 	)
 }
