c23ec6e29489552efdfa82307c5be91848812f79
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-28 17:24:11 +0200

    added div

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,12 +11,12 @@ function App() {
 	}
 
 	return (
-		<>
+		<div>
 			<input type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
 			<input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
 			<input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
 			<button onClick={handleSubmit}></button>
-		</>
+		</div>
 	)
 }
 
