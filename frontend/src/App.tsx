b4ebdbef3f948f15e0a21c44abf9919c0ff55eb0
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-28 17:20:31 +0200

    test form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [username, setUsername] = useState("")
+	const [password, setPassword] = useState("")
+	const [email, setEmail] = useState("")
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	async function handleSubmit() {
+		console.log(username, password, email)
+	}
+
+	return (
+		<>
+			<input type="text" placeholder="Username" onChange={e => setUsername(e.target.value)} />
+			<input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+			<input type="email" placeholder="Email" onChange={e => setEmail(e.target.value)} />
+			<button onClick={handleSubmit}></button>
+		</>
+	)
 }
 
 export default App
