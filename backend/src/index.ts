a4421865050eef05bec03c6f8c4a3a098132ded1
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-29 12:41:51 +0200

    added var for discord config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,10 +7,12 @@ import { exit } from "process"
 import type { DiscordClientConfig } from "./types/discord"
 require("dotenv").config()
 
+const discordConfigOK = discordConfig.token && discordConfig.clientId && discordConfig.clientSecret
+
 // Check for required environment variables
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL not set")
 if (!process.env.TOKEN_SECRET) throw new Error("TOKEN_SECRET not set")
-if (!discordConfig.token || !discordConfig.clientId || !discordConfig.clientSecret) console.warn("Incorrect Discord config!")
+if (!discordConfigOK) console.warn("Incorrect Discord config!")
 if (!process.env.PORT) console.warn("PORT not set, using default 4000")
 
 // Prisma client
@@ -18,7 +20,7 @@ const prisma = new PrismaClient({ errorFormat: "pretty" })
 
 // Discord client
 let discordBot: DiscordBot | undefined
-if (discordConfig.token && discordConfig.clientId && discordConfig.clientSecret) {
+if (discordConfigOK) {
 	const config = discordConfig as DiscordClientConfig
 	discordBot = new DiscordBot(config)
 }
