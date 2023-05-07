ab1928ab00ecae187b7df94a7f6905a100f44fd1
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-05-07 14:47:09 +0200

    fixed type error

--- frontend/src/types/index.d.ts
+++ frontend/src/types/index.d.ts
@@ -4,8 +4,43 @@ interface defaultProps {
 	setUser: React.Dispatch<React.SetStateAction<User>>
 }
 
+type BasicAPIResponse<T> = BasicAPIResponseError | BasicAPIResponseNoError<T>
+
+type BasicAPIResponseError = {
+	type: "error"
+	error: string
+}
+
+type BasicAPIResponseNoError<T> = {
+	type: "success"
+	message: string
+	data: T
+}
+
 interface User {
 	id: string
 	name: string
 	email: string
+	admin: number
+}
+
+interface Bot {
+	id: string
+	name: string
+	activeChatCount: number
+	brain: Omit<Brain, "data">
+	serviceAccess: ServiceAccess
+	socketPort: number
+}
+
+interface Brain {
+	id: string
+	name: string
+	data: string
+}
+
+interface ServiceAccess {
+	discord: boolean
+	mastodon: boolean
+	slack: boolean
 }
