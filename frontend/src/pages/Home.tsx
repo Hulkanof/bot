26a7de7b6dd6857a41ad2904aae2246b051acd9d
Cyril Girardin <cyrilhulkanof@gmail.com>
2023-05-28 17:19:18 +0200

    Side Menu test

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import useToken from "../hooks/useToken"
+import { SideBarMenuItem } from "../types/SideBarMenu"
+import { FcAdvertising } from "react-icons/fc"
+import { SideBarMenu } from "../components/SideBarMenu"
 
 const Home: React.FC<defaultPageProps> = props => {
 	const { user } = props
@@ -10,8 +13,24 @@ const Home: React.FC<defaultPageProps> = props => {
 	useEffect(() => {
 		if (!token) navigate("/login")
 	}, [])
+	
+	const items: SideBarMenuItem[] = [
+		{
+			id: "1",
+			label: "Home",
+			icon : FcAdvertising,
+			url : "/"
+		},
+	];
+		
+	if (!token) navigate("/login")
+
 	return (
+		
 		<div>
+			<div>
+			<SideBarMenu items={items}/>
+			</div>
 			<h1>Welcome!</h1>
 			<div>
 				<div>{user.name}</div>
